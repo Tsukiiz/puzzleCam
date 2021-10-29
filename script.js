5ad69817f62f5d7df3ca25687685e0477925ed71
Tsuki <kanin.twcr@gmail.com>
2021-10-29 16:14:19 +0700

    Auto update when resize window

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ let SIZE = {x:0, y:0, width:0, heigh:0};
 function main(){
     CANVAS = document.getElementById("myCanvas");
     CONTEXT = CANVAS.getContext("2d");
-    CANVAS.width = window.innerWidth;
-    CANVAS.height = window.innerHeight;
+    
     let promise = navigator.mediaDevices.getUserMedia({video:true});
     promise.then(function(signal){
         VIDEO = document.createElement("video");
@@ -16,20 +15,27 @@ function main(){
         VIDEO.play();
 
         VIDEO.onloadeddata = function(){
-            let resizer = SCALER*
-                Math.min(
-                    window.innerWidth/VIDEO.videoWidth,
-                    window.innerHeight/VIDEO.videoHeight
-                );
-            SIZE.width = resizer * VIDEO.videoWidth;
-            SIZE.height = resizer * VIDEO.videoHeight;
-            SIZE.x = window.innerWidth/2-SIZE.width/2;
-            SIZE.y = window.innerHeight/2-SIZE.height/2;
+            handleResize();
+            window.addEventListener('resize',handleResize);
             updateCanvas();
         }
     }).catch(function(err){
         alert("Camera error: " + err);
     });
+}
+
+function handleResize(){
+    CANVAS.width = window.innerWidth;
+    CANVAS.height = window.innerHeight;
+    let resizer = SCALER*
+                Math.min(
+                    window.innerWidth/VIDEO.videoWidth,
+                    window.innerHeight/VIDEO.videoHeight
+                );
+    SIZE.width = resizer * VIDEO.videoWidth;
+    SIZE.height = resizer * VIDEO.videoHeight;
+    SIZE.x = window.innerWidth/2-SIZE.width/2;
+    SIZE.y = window.innerHeight/2-SIZE.height/2;
 }
 
 function updateCanvas(){
